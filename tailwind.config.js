abb56b689f7134ef0c58b67b06ec59acfc4cd590
Clara Castillo <claracastillochale@gmail.com>
2023-11-22 14:51:07 +0100

    style: run prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", // add this line
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
